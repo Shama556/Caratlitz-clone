86611e6c4c8bc2c2071c7f00e56a4e34ea2e9d55
Shama556 <shamaparveen5297@gmail.com>
2024-09-11 21:38:01 +0530

    Add files via upload

---
+++ var massage;.js
@@ -0,0 +1,45 @@
+// var massage;
+
+// massage = "Hyyy SHAMA."
+
+// console.log(massage);
+// console.log(massage);
+// console.log(massage);
+
+// let text;
+
+// text = "Shama";
+
+// console.log(text);
+
+const remainder = number %2;
+
+if (remainder === 0){
+    console.log( `$ {number} is a even number`)
+      if(number % 4 === 0){
+        console.log (`${number} is divisible by 4`)  
+      }
+      else{
+        console.log(`${number} is nit divisible by 4`)
+      }
+}
+
+else{
+    console.log(`${number} is odd number `)
+}
+
+   
+
+
+
+
+
+
+
+
+
+let studentName = ["shama1", "shama2", "shama3", "shama4", "shama5"];
+
+let arrlength = studentName.length;
+
+console.log(arrlength);
